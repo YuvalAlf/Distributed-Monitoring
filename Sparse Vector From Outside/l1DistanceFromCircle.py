aea824b730c0310f97931984dbbd4a66df7deedd
unknown <u67v67@gmail.com>
2018-08-24 12:29:24 +0300

    L0 Minimization Presentation

--- Sparse Vector From Outside/l1DistanceFromCircle.py
+++ Sparse Vector From Outside/l1DistanceFromCircle.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import cvxpy as cvx
 import numpy
@@ -5,7 +6,7 @@ import numpy
 def distanceL1FromCircle(threshold, point, solver, options):
     point = numpy.array(point, dtype=float)
     x = cvx.Variable(len(point))
-    objective = cvx.Minimize(cvx.norm(x - point, 1)) 
+    objective = cvx.Minimize(cvx.norm(x - point, 1))
     constraints = [cvx.norm(x, 2) <= numpy.sqrt(threshold)]
     prob = cvx.Problem(objective, constraints)
     try:
@@ -13,44 +14,37 @@ def distanceL1FromCircle(threshold, point, solver, options):
         # Ignore suboptimal solutions
         if prob.status != 'optimal':
             return '-', prob.status
-        # CVXPY versions below 1.0 return numpy.matrx instead of array so convert
         solution = numpy.asarray(x.value).squeeze()
-        # Make sure solution is in the feasible set or warn if not
-        distance = solution.dot(solution) - threshold 
-        if distance > 1e-4:
-            print('*** WARNING for solver', solver, ': solution is', abs(distance), 'above threshold')
-        # Return 
-        return prob.value, 'optimal'
+        distanceVector = [0 if abs(val) < 1e-5 else val for val in point - solution]
+        l0 = sum([val != 0 for val in distanceVector])
+        return l0, 'optimal'
     except cvx.SolverError:
         return '-', 'failed'
-    
 
-def treatLine(line):
-    tokens = line.replace('\n', '').replace('\r', '').split(' ')
-    threshold = float(tokens[1])
-    point = list(map(float, tokens[2:]))
-    sys.stdout.write('started computing\n')
-    result = distanceL1FromCircle(threshold, point)
-    sys.stdout.write(str(result) + '\n')
-    sys.stdout.flush()
+def realDimensionReduction(point, threshold):
+    sqr = lambda x: x * x
+    pointSorted = sorted(point, key=abs, reverse=True)
+    sumSquarred = sum(sqr(x) for x in point)
+    dimension = 0
+    while True:
+        if sumSquarred <= threshold:
+            return dimension
+        sumSquarred -= sqr(pointSorted[dimension])
+        dimension += 1
+
 
 if __name__=='__main__':
     # Solvers and parameters
-    solver_options = [ ('SCS', dict(eps=1e-5, max_iters=100000)), 
-                       ('ECOS',  dict(max_iters=1000)),
-                       ('CVXOPT',  dict(kktsolver=cvx.ROBUST_KKTSOLVER, max_iters=1000))
-                     ]
-    for i in range(30):
-        value = 2 ** i
-        dimesion = 10
-        point = [value for _ in range(dimesion)]
-        diff = 1.0
-        threshold = sum([v * v for v in point]) - diff
-        print('iteration', i, 'threshold: ', threshold)
-        for solver, options in solver_options:
-            objective, status = distanceL1FromCircle(threshold, point, solver, options)
-            print('    {}: {} ({})'.format(solver, objective, status))
-        print()
-        
+    solver, solverOptions = 'SCS', dict(eps=1e-7, max_iters=1000000)
+    print('Dimension;Ratio;L0 of L1 Distance;Real L0 Distance')
+    #for dimension in numpy.linspace(100, 1000, 11): # 91
+    for dimension in [100000]:
+        point = [random.uniform(0.0, 1.0) for _ in range(dimension)]
+        pointFunctionValue = sum([v * v for v in point])
+        for ratio in numpy.linspace(0.9, 0.999, 100): # 100
+            threshold = pointFunctionValue * ratio
+            l0OfDistance, status = distanceL1FromCircle(threshold, point, solver, solverOptions)
+            print('{0};{1};{2};{3}'.format(dimension, ratio, l0OfDistance, realDimensionReduction(point, threshold)))
+
 
 
