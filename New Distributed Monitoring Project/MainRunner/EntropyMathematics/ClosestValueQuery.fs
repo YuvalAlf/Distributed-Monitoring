0394cad5ba06d9e38c5fcf08524957ac7c90369e
unknown <u67v67@gmail.com>
2019-08-02 09:43:42 +0300

    Closest Value Query Data Structure

---
+++ New Distributed Monitoring Project/MainRunner/EntropyMathematics/ClosestValueQuery.fs
@@ -0,0 +1,49 @@
+﻿namespace EntropyMathematics
+
+open System
+
+type Tree<'t when 't :> IComparable<'t>> = { Data : 't array
+                                             Left : Option<Tree<'t>>
+                                             CurrentIndex : int
+                                             Right : Option<Tree<'t>>} with
+    member tree.CurrentValue = tree.Data.[tree.CurrentIndex]
+    member tree.PreviousValue = tree.Data.[tree.CurrentIndex - 1]
+    member tree.GetClosestSmallerValue(value : 't) =
+        let x = tree.CurrentValue
+        match tree.Left, tree.CurrentValue.CompareTo(value), tree.Right with
+        | _,              0, _ -> value
+        | None,           d, _ when d > 0 -> tree.PreviousValue
+        | Some(leftNode), d, _ when d > 0 -> leftNode.GetClosestSmallerValue(value)
+        | _,              d, None when d < 0 -> tree.CurrentValue
+        | _,              d, Some(rightNode) when d < 0 -> rightNode.GetClosestSmallerValue(value)
+        | otherwise -> failwith "Programming Error"
+
+    static member InitClosestValueQuery(values : 't array) : Tree<'t> =
+        let values = values |> Array.sortWith (fun x y -> (x :> IComparable<'t>).CompareTo(y))
+        let rec createTreeReq (startingIndex : int) (endingIndex : int) : Option<Tree<'t>> = 
+            if startingIndex > endingIndex then
+                None
+            else
+                let nodeIndex = startingIndex + (endingIndex - startingIndex) / 2
+                let left = createTreeReq startingIndex (nodeIndex - 1)
+                let right = createTreeReq (nodeIndex + 1) endingIndex
+                Some ({Data = values; Left = left; CurrentIndex = nodeIndex; Right = right})
+
+        createTreeReq 0 (values.Length - 1)
+        |> Option.get
+
+    static member InitExponentialClosestValueQuery (startValue : int64, max : int64, mulFactor : double) =
+        Tree.InitClosestValueQuery
+            [|
+                yield 0L
+                let generator value =
+                    let nextValue = Math.Floor(double(value) * mulFactor) |> int64
+                    if value > max then None else Some(value, nextValue)
+                yield! (startValue |> Seq.unfold generator)
+
+            |]
+
+module ClosestValueQuery =
+    let Init<'t when 't :> IComparable<'t>> values = Tree<'t>.InitClosestValueQuery values
+    let InitExponential (startValue : int64, max : int64, mulFactor : double) =
+        Tree<int64>.InitExponentialClosestValueQuery (startValue, max, mulFactor)
---
+++ New Distributed Monitoring Project/MainRunner/UnitTests/ClosestValueQueryTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using EntropyMathematics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utils.TypeUtils;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ClosestValueQueryTests
+    {
+        [TestMethod]
+        public void GeneralTest()
+        {
+            var query = ClosestValueQuery.Init(ArrayUtils.Init(1,6,9,20, -20));
+            Assert.AreEqual(1, query.GetClosestSmallerValue(5));
+            Assert.AreEqual(20, query.GetClosestSmallerValue(100));
+            Assert.AreEqual(9, query.GetClosestSmallerValue(9));
+            Assert.AreEqual(9, query.GetClosestSmallerValue(10));
+            Assert.AreEqual(9, query.GetClosestSmallerValue(13));
+            Assert.AreEqual(-20, query.GetClosestSmallerValue(-5));
+            Assert.ThrowsException<IndexOutOfRangeException>(() => query.GetClosestSmallerValue(-100));
+        }
+        [TestMethod]
+        public void ExponensialGeneratorTest()
+        {
+            var query = ClosestValueQuery.InitExponential(2, 65556, 2.0);
+            Assert.AreEqual(0, query.GetClosestSmallerValue(1));
+            Assert.AreEqual(0, query.GetClosestSmallerValue(0));
+            Assert.AreEqual(2, query.GetClosestSmallerValue(3));
+            Assert.AreEqual(2, query.GetClosestSmallerValue(2));
+            Assert.AreEqual(8, query.GetClosestSmallerValue(15));
+            Assert.AreEqual(16, query.GetClosestSmallerValue(16));
+            Assert.AreEqual(256, query.GetClosestSmallerValue(333));
+            Assert.AreEqual(512, query.GetClosestSmallerValue(777));
+            Assert.AreEqual(512, query.GetClosestSmallerValue(512));
+            Assert.AreEqual(512, query.GetClosestSmallerValue(1023));
+            Assert.ThrowsException<IndexOutOfRangeException>(() => query.GetClosestSmallerValue(-100));
+        }
+        
+    }
+}
